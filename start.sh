800f5021dd3c8b783904e93872e12ff5cd7032f3
tjdalsckd <53217819+tjdalsckd@users.noreply.github.com>
2021-06-24 10:50:57 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 xhost +local:root
- docker  run  --rm -it --name gpu_voxels --privileged  --volume=/dev:/dev --gpus all --net=host -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -e QT_X11_NO_MITSHM=1 tjdalsckd/gpu_voxels:latest bash
+ docker  run  --rm -it --name gqcnn_smc --privileged  --volume=/dev:/dev --gpus all --net=host -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -e QT_X11_NO_MITSHM=1 tjdalsckd/gqcnn_smc:latest bash
